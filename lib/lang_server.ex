250b6722752a86684c7b523bbf09621117936810
Benjamin Tan Wei Hao <benjamintanweihao@gmail.com>
2015-05-17 23:25:23 +0800

    Add LangServer to read from config/languages.json.

---
+++ lib/lang_server.ex
@@ -0,0 +1,21 @@
+defmodule Octex.LangServer do
+
+  def start_link do
+    Agent.start_link(fn -> load_languages end, name: __MODULE__)
+  end
+
+  def get_languages do
+    Agent.get(__MODULE__, fn langs -> langs |> Map.keys end)
+  end
+
+  def get_language(lang) do
+    Agent.get(__MODULE__, fn langs -> langs |> Map.get(lang) end)
+  end
+
+  defp load_languages do
+    {:ok, file} = File.read("config/languages.json")
+    {:ok, json} = file |> Poison.decode
+    json
+  end
+
+end
